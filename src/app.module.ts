cd1fb9fce1458b526584422e15ccfe18e5fa3981
Gabriel Turcheti <gturcheti@MacBook-Air-de-Gabriel.local>
2025-01-15 23:41:23 -0300

    feat: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+const DATABASE_PORT = parseInt(process.env.DATABASE_PORT, 10);
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'mypostgres',
-      password: '123456',
-      database: 'blogging',
+      host: process.env.DATABASE_HOST,
+      port: DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: process.env.NODE_ENV === 'development',
     }),
     BlogModule,
   ],
