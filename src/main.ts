55337f1f874944bcfb1451bf9e5700f3d3f883e2
Gabriel Turcheti <gturcheti@MacBook-Air-de-Gabriel.local>
2025-03-24 18:24:00 -0300

    fix: deals with cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
     .setTitle('Tech Challenge Blog API - DOC')
